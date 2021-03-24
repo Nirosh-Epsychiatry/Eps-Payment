b290b675351ab6ebf55b70a32dc17ddb3b6c1435
kpnlakmal <64244041+kpnlakmal@users.noreply.github.com>
2021-03-24 15:18:10 +0530

    added to github

---
+++ src/main/java/com/epspayment/EpspaymentApplication.java
@@ -0,0 +1,13 @@
+package com.epspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpspaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpspaymentApplication.class, args);
+    }
+
+}
